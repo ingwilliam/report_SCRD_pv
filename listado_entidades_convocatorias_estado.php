5ca8a2a48dd335e6e0e958e9b70176e9e5717f18
Ing William Barbosa <ingeniero.wb@gmail.com>
2020-04-07 08:19:30 -0500

    ACTUALIZACIÓN DEL LOGO

--- listado_entidades_convocatorias_estado.php
+++ listado_entidades_convocatorias_estado.php
@@ -33,7 +33,7 @@ class MYPDF extends TCPDF {
         //Page header
         public function Header() {
                 // Logo
-                $image_file = 'images/scrd_logo.png';
+                $image_file = 'images/scrd_logo_2.png';
                 
                 $this->Image($image_file, 145, 5, 17, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
                 
